1258596633c57a70b1253adc330e5402ee524165
DonJNewman <Dnewman1934@yahoo.com>
2025-04-06 12:30:11 -0500

    successful line drawing, seperation of graphics code from main

---
+++ graphics.py
@@ -0,0 +1,46 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window: #ROOT, WHICH IS THE BASE TK, CANVAS, WHICH IS THE GUI BOX, ISRUNNING FOR LOGIC
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("MazeSolver :D")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand = 1)
+        self.__windowIsRunning = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    # REDRAW WAITFORCLOSE AND CLOSE ARE LOGIC METHODS TO KEEP WINDOW RUNNING
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+
+    def wait_for_close(self):
+        self.__windowIsRunning = True
+        while self.__windowIsRunning == True:
+            self.redraw()
+
+
+            
+    #DRAWING THE LINES FOR MAZE GENERATION
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+    def close(self):
+        self.__windowIsRunning = False
+
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, pta, ptb):
+        self.pta = pta
+        self.ptb = ptb
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.pta.x, self.pta.y, self.ptb.x, self.ptb.y, fill=fill_color, width=2)
+
--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 from tkinter import Tk,BOTH, Canvas
-
+from graphics import Window, Line, Point
 def main():
     win = Window(800,600)
     win.wait_for_close()
-    return None
-
-
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("MazeSolver :D")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand = 1)
-        self.__windowIsRunning = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-
-    def wait_for_close(self):
-        self.__windowIsRunning = True
-        while self.__windowIsRunning == True:
-            self.redraw()
-    
-    def close(self):
-        self.__windowIsRunning = False
+    l = Line(Point(25,30), Point(444,666))
+    win.draw_line(l,"black")
+    win.wait_for_close()
 
 
 if __name__ == "__main__":
