4bcba1ca40a548c7569ea12f7687683a91945a69
DonJNewman <Dnewman1934@yahoo.com>
2025-04-06 12:31:54 -0500

    double wait fix

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from tkinter import Tk,BOTH, Canvas
 from graphics import Window, Line, Point
 def main():
     win = Window(800,600)
-    win.wait_for_close()
     l = Line(Point(25,30), Point(444,666))
     win.draw_line(l,"black")
+    win.draw_line(Line(Point(10,20), Point(10,600)),"black")
     win.wait_for_close()
 
 
